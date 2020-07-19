6074a5f6722153089146662b73c0970cacb1877d
kpeter74 <kisvaradi.peter@gmail.com>
2020-07-19 17:32:59 +0200

    Updates

---
+++ precache-manifest.00c592acb6acc730a4fe89a79ea6b8af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cfdeb64828e7d4894ec38d311235d0",
+    "url": "/monstesr-rolodex/index.html"
+  },
+  {
+    "revision": "a155365c368630329bef",
+    "url": "/monstesr-rolodex/static/css/main.ab90fae0.chunk.css"
+  },
+  {
+    "revision": "3f7db5ef6cdc34d1c348",
+    "url": "/monstesr-rolodex/static/js/2.6662c0af.chunk.js"
+  },
+  {
+    "revision": "a155365c368630329bef",
+    "url": "/monstesr-rolodex/static/js/main.69f25e3e.chunk.js"
+  },
+  {
+    "revision": "fb5b2a5168c58be24fe9",
+    "url": "/monstesr-rolodex/static/js/runtime~main.e0b99797.js"
+  }
+]);
